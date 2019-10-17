313d690c447f1f4d1ec8d5d4ada1fc0900333405
Asher Mancinelli <ashermancinelli@gmail.com>
2019-10-16 18:11:57 -0700

    test: check for pdo

--- tests/DatabaseTests.php
+++ tests/DatabaseTests.php
@@ -5,6 +5,12 @@ use PHPUnit\Framework\TestCase;
 class DatabaseTests extends TestCase
 {
     /** @test */
+    public function TestHasPDO()
+    {
+        $this->assertTrue(defined('PDO::ATTR_DRIVER_NAME'));
+    }
+
+    /** @test */
     public function TestOpenDB()
     {
         $this->assertTrue(true);
